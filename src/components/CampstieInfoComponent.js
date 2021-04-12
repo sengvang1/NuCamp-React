84ce07c26193862f5461ddb6ef56d41e45ae00fe
Seng Vang <seng.vang1@gmail.com>
2021-04-11 20:55:29 -0500

    Main Component Added

--- src/components/CampstieInfoComponent.js
+++ src/components/CampstieInfoComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class CampsiteInfo extends Component {
 
@@ -33,10 +33,14 @@ class CampsiteInfo extends Component {
     render() {
 
         if (this.props.campsite) {
-            return (<div className="row">
-                {this.renderCampsite(this.props.campsite)}
-                {this.renderComments(this.props.campsite.comments)}
+            return (
+            <div className="container">
+                <div className="row">
+                    {this.renderCampsite(this.props.campsite)}
+                    {this.renderComments(this.props.campsite.comments)}
+                </div>
             </div>)
+
         } 
 
         return (<div/>);
