7f709125d1def5b2facc302e048026af4d985003
Seng Vang <seng.vang1@gmail.com>
2021-04-22 20:53:36 -0500

    Intro to Redux

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-import { CAMPSITES } from '../shared/campsites';
 import CampsiteInfo from './CampstieInfoComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -8,33 +7,29 @@ import Contact from './ContactComponent';
 import Home from './HomeComponent';
 import About from './AboutComponent';
 import TestDirectory from './TestDirectory';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { COMMENTS } from '../shared/comments';
-import { PARTNERS } from '../shared/partners';
-import { PROMOTIONS } from '../shared/promotions';
+import { Switch, Route, Redirect, withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 
+const mapStateToProps = state => {
+  return {
+    campsites: state.campsites,
+    comments: state.comments,
+    partners: state.partners,
+    promotions: state.promotions
+  }
+}
 
 class Main extends Component {
-  constructor (props) {
-    super(props);
-    this.state = {
-      campsites: CAMPSITES,
-      comments: COMMENTS,
-      partners: PARTNERS,
-      promotions: PROMOTIONS,
-      // currentCamp: null     
-    };
-  }
-
+  
       render() {
 
       const HomePage = () => {
         return (
           <Home
-            campsite={this.state.campsites.filter(campsite => campsite.featured)[0]} 
-            promotion={this.state.promotions.filter(promotion => promotion.featured)[0]} 
-            partner={this.state.partners.filter(partner => partner.featured)[0]} 
+            campsite={this.props.campsites.filter(campsite => campsite.featured)[0]} 
+            promotion={this.props.promotions.filter(promotion => promotion.featured)[0]} 
+            partner={this.props.partners.filter(partner => partner.featured)[0]} 
           />
         );
       }
@@ -48,25 +43,25 @@ class Main extends Component {
       
         return (
           <CampsiteInfo
-            campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
-            comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
+            campsite={this.props.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+            comments={this.props.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)} 
             />
         );
       };
 
-      console.log(this.state.campsites);
+      console.log(this.props.campsites);
 
       return (
       <div>
         <Header />
         <Switch>
           <Route path='/home' component={HomePage} />
-          <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+          <Route exact path='/directory' render={() => <Directory campsites={this.props.campsites} />} />
           <Route path='/directory/:campsiteId' component={CampsiteWithId} /> 
           <Route exact path='/contactus' component={Contact} />
-          <Route exact path='/aboutus' render={() => <About partners={this.state.partners} />} />
+          <Route exact path='/aboutus' render={() => <About partners={this.props.partners} />} />
           {/* <Route exact path='/testpage'component={TestPage} /> */}
-          <Route exact path='/testdirectory' render={() => <TestDirectory campsites={this.state.campsites}/> } />
+          <Route exact path='/testdirectory' render={() => <TestDirectory campsites={this.props.campsites}/> } />
           <Redirect to='/home' />
         </Switch>
         <Footer />
@@ -77,4 +72,4 @@ class Main extends Component {
   
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
